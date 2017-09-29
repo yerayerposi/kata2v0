089bd76ff59e1c6e8777aee83e2b0700e8054d0d
usuario <usuario@10.22.148.62>
2017-09-29 09:23:25 +0100

    kata2 v2

--- src/histograma/Histograma.java
+++ src/histograma/Histograma.java
@@ -21,16 +21,12 @@ public class Histograma {
         Map<Integer,Integer> histogram = new HashMap<>();
     
 
-    for (int i = 0; i < data.length; i++) {
-    if(histogram.containsKey(data[i])) {
-        histogram.put(data[i], histogram.get(data[i]) + 1);
-        
-    }else{
-        histogram.put (data[i], 1);        
-    }
-   
-    }
+        for (int value : data) {
+            histogram.put(value,histogram.containsKey(value) ? histogram.get(value) + 1 : 1);
+        }
+       
     for(int key : histogram.keySet()){
-        System.out.println(key + " -->" + histogram.get(key));
+            System.out.println(key + " -->" + histogram.get(key));
+        }
     }
-}}
+}
